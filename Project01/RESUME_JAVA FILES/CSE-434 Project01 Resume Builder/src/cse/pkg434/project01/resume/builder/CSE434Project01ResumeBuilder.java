1f9332012ee7cbfe3347fbf28557a023fbb81d92
HafizurRahman111 <rahman.hafizur@northsouth.edu>
2019-06-24 15:09:51 +0600

    Resume Builder Java File With Name Input Field

--- Project01/RESUME_JAVA FILES/CSE-434 Project01 Resume Builder/src/cse/pkg434/project01/resume/builder/CSE434Project01ResumeBuilder.java
+++ Project01/RESUME_JAVA FILES/CSE-434 Project01 Resume Builder/src/cse/pkg434/project01/resume/builder/CSE434Project01ResumeBuilder.java
@@ -21,11 +21,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.layout.StackPane;
 
 import javafx.scene.control.ListView; 
-import javafx.scene.control.TextField;
 
 import javafx.scene.text.Text; 
 import javafx.scene.text.Font; 
@@ -34,7 +31,6 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;  
 
 import javafx.scene.paint.Color; 
-
 
 
 
@@ -48,22 +44,67 @@ import javafx.scene.paint.Color;
     @Override
      public void start(Stage stage) throws Exception
     {
-      Text text = new Text();              //Text object Created here
-      Group form_gui = new Group(text);   // Group object created here  
-      Scene scene = new Scene(form_gui, 850, 700);  // Scene object Created Here
+        
+      Text title = new Text();                   //Text object Created for Title here
+      
+      Text contactinfo = new Text();             // Text Object for Contact Information created
+      
+      Text name = new Text();                    // Text Object for Name created
+      TextField nameField = new TextField();     // Input Field Object created for Name  
+      
+      
+      
+      
+      Group form_gui = new Group(title, contactinfo, name,nameField );   // Group object created here 
+      
+      Scene scene = new Scene(form_gui, 850, 700);                       // Scene object Created Here
       
       stage.setTitle("CSE-434 Project01 Resume Builder by Hafizur Rahman");  // Set the title for Stage
       
-      text.setText("RESUME BUILDER");  // Title Text
-      text.setX(350);       //Position of the text Title
-      text.setY(30);
-      text.setFont(Font.font("Times New Roman",FontPosture.REGULAR,20));//Font Settings for Text Title 
-              
-      text.setFill(Color.BLACK);    //Color Fill inside the text title 
-      text.setStrokeWidth(2);       //  Stroke size 
-      text.setStroke(Color.GREEN);     // Color of stroke     
       
-    
+      
+      // Title    - RESUME BUILDER 
+      
+      title.setText("RESUME BUILDER");  // Title Text
+      title.setX(350);                  //Position of the text Title
+      title.setY(25);
+      title.setFont(Font.font("Times New Roman",FontPosture.REGULAR,20));//Font Settings for Text Title 
+              
+      title.setFill(Color.BLACK);       //Color Fill inside the text title 
+      title.setStrokeWidth(2);          //  Stroke size 
+      title.setStroke(Color.GREEN);     // Color of stroke     
+            
+      
+     // Contact Informations 
+      
+      contactinfo.setText("Contact Information :");  // Contact information Text Title Here
+     
+      contactinfo.setX(20);                //Position of the contact information text 
+      contactinfo.setY(75);
+      contactinfo.setFont(Font.font("Times New Roman",FontPosture.REGULAR,18));//Font Settings for Text Title 
+      contactinfo.setStrokeWidth(1);       //  Stroke size 
+      contactinfo.setUnderline(true);  
+      
+      
+      
+      
+      // Name 
+      name.setText("Name :");  // Name Text
+      name.setX(60);           //Position of the name text 
+      name.setY(116);
+      name.setFont(Font.font("Arial",FontPosture.REGULAR,16)); //Font Settings for Name Text  
+               
+      // Input Field For Name
+      
+      nameField.setLayoutX(120);  // Position setup for Name Input Field
+      nameField.setLayoutY(100);
+     
+       
+      
+  
+       
+       
+       
        
       stage.setScene(scene);    // Scene added in stage       
       stage.show();             //  Stage elements show 
